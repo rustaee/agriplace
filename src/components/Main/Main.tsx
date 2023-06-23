1f46c926836e9eea0d93c711f83cc4397363f58b
mahdieh <mahdieh.roustaei@gmail.com>
2023-06-23 16:39:15 +0200

    show products and the details in main page

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 import Layout from "../../layouts/main/main.layout";
 import { Link } from "react-router-dom";
+import { useGetFruitsQuery } from "app/api";
+import { Fruit } from "types/Fruit";
+import { useState } from "react";
 
 const App = styled.div`
   display: grid;
@@ -12,32 +15,67 @@ const App = styled.div`
   .products {
     grid-area: products;
     background-color: lightblue;
+    padding: 20px;
   }
 
   .product-view {
+    padding: 20px;
+
     grid-area: product-view;
   }
 
   .cart {
+    padding: 20px;
+
     grid-area: cart;
     background-color: lightgray;
   }
 
   .recent-products {
+    padding: 20px;
+
     grid-area: recent;
     background-color: lightgreen;
   }
 `;
 
 export const Main = () => {
+  const { isError, isLoading, isFetching, data: fruits } = useGetFruitsQuery();
+  const [selectedFruit, setSelectedFruit] = useState<Fruit>();
+
   return (
     <App>
       <div className="products">
         <h1>
-          <Link to="/admin">Dashboard</Link>
+          <Link to="/admin" style={{ color: "red" }}>
+            Dashboard
+          </Link>
         </h1>
+        {isLoading && "Loading..."}
+        {fruits?.map((fruit) => (
+          <div key={fruit.id}>
+            <h3
+              onClick={() => setSelectedFruit(fruit)}
+              style={{ cursor: "pointer" }}
+            >
+              {fruit.name}
+            </h3>
+          </div>
+        ))}
       </div>
-      <div className="product-view"></div>
+      <div className="product-view">
+        {!selectedFruit && <h1>Select a fruit to see the details</h1>}
+        {selectedFruit && (
+          <div>
+            <h1>{selectedFruit.name}</h1>
+            <h3>{selectedFruit.description}</h3>
+            <strong>Tags: </strong>
+            {selectedFruit.tags?.map((tag, index) => (
+              <span key={index}>{tag} - </span>
+            ))}
+          </div>
+        )}
+      </div>
       <div className="cart"></div>
       <div className="recent-products"></div>
     </App>
